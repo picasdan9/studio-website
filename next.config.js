3f021479b592024a236432529b033e5b91b814b5
Dan Tran <picasdan99@gmail.com>
2021-12-01 14:22:25 +0800

    Update next config

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  reactStrictMode: true,
-  basePath: isProd ? '/night-personal-site' : '',
-  assetPrefix: isProd ? '/night-personal-site/' : '',
-};
