4a146c06d0a76c53685fc575caa2f77faecde71d
Dan Tran <picasdan99@gmail.com>
2022-05-29 01:34:40 +0200

    Refactor navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,17 +1,51 @@
 import { GetIsMobile } from 'components/utils';
-import React from 'react';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
 
-import NavbarDesktop from './NavbarDesktop';
-import NavbarMobile from './NavbarMobile';
+import { navButtons } from './constants';
+import NavbarStyles from './NavbarStyles.module.css';
 
 const Navbar: React.FC<Object> = () => {
   const isMobile = GetIsMobile();
-  return isMobile === null ? (
-    <div />
-  ) : isMobile === true ? (
-    <NavbarMobile />
-  ) : (
-    <NavbarDesktop />
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  useEffect(() => {
+    setIsExpanded(false);
+  }, [isMobile]);
+
+  function computeNavButClassName() {
+    if (isMobile !== null) {
+      // hide navbar-button only when the viewport is mobile-sized and navbar is not expanded
+      if (isExpanded || !isMobile) return NavbarStyles['navbar-button'];
+      return NavbarStyles['navbar-button'] + ' hidden';
+    }
+  }
+
+  return (
+    <nav>
+      {isMobile === null ? (
+        <div />
+      ) : (
+        <ol className={!isMobile ? NavbarStyles['navbar-desktop'] : ''}>
+          <li
+            className={NavbarStyles['navbar-button']}
+            onClick={() => isMobile && setIsExpanded(!isExpanded)}
+          >
+            <Link href=''>
+              <a>dan n. tran</a>
+            </Link>
+          </li>
+          {navButtons.map(({ label, path }) => (
+            <li key={path} className={computeNavButClassName()}>
+              <Link href={path}>
+                <a>{label}</a>
+              </Link>
+            </li>
+          ))}
+        </ol>
+      )}
+    </nav>
   );
 };
 
