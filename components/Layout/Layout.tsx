752a14840d5fb18f03d359d09d661bbe5f9fecec
Dan Tran <picasdan99@gmail.com>
2022-05-29 22:00:54 +0200

    Simplify layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,25 +1,21 @@
+import Navbar from 'components/Navbar';
 import Head from 'next/head';
 import React from 'react';
-import styles from './LayoutStyles.module.css';
-
-import Navbar from 'components/Navbar';
 
 const Layout: React.FC<{ title: string; children: React.ReactNode }> = ({
   title,
   children,
 }) => (
-  <div className='Layout'>
+  <>
     <Head>
       <title>{title}</title>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta charSet='utf-8' />
     </Head>
 
-    <main className={styles.main}>
-      <Navbar />
-      <div className={styles['page-container']}>{children}</div>
-    </main>
-  </div>
+    <Navbar />
+    <main>{children}</main>
+  </>
 );
 
 export default Layout;
